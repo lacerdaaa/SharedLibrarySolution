cca28904ae832722e5db9cf48804fed15095673b
20240525 <20240525@LABM01-03>
2025-05-27 21:23:00 -0300

    Adicionar arquivos de projeto.

---
+++ Shared.Lib/Models/SharedData.cs
@@ -0,0 +1,22 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Shared.Lib.Models
+    {
+        public class SharedData
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = string.Empty;
+
+            private DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+                
+            public string GetInfo()
+        {
+            return $"Id: ${Id}, Name: ${Name}, CreatedAt: ${CreatedAt}";
+        }      
+                        
+        }
+    }
---
+++ Shared.Lib/Services/SharedCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Lib.Services
+{
+    public class SharedCalculator
+    {
+        public double Add(double a, double b) => a + b;
+        
+        public double Subtract(double a, double b) => a - b;
+
+        public double Divide (double a, double b)
+        {
+            if(b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero.");
+            }   
+        }
+
+    }
+}
